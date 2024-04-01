64b0adb90e3a8045e7d5b7f3971b59785b02b92f
shubrajyoti7 <shubrajyotichoudhury7@gmail.com>
2024-04-01 22:38:19 +0530

    my first c code

---
+++ first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter an element:");
+    scanf("%d",&a);
+    printf("\nenter an element:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("\n%d",c);
+    return 0;
+}
